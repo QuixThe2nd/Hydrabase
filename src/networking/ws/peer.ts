8a27308ab40fa2b359da7199c720c26cd7c5b484
Parsa Yazdani <parsa@yazdani.au>
2026-02-20 20:20:27 +1100

    request timeouts

--- src/networking/ws/peer.ts
+++ src/networking/ws/peer.ts
@@ -54,17 +54,33 @@ export class Peer {
   }
 
   public async sendRequest<T extends Request['type']>(request: Request & { type: T }): Promise<Response<T>> {
-    if (!this.socket.isOpened) {
-      console.warn('WARN:', `Cannot send request to unconnected peer ${this.socket.address}`)
-      return []
-    }
-    this.nonce++;
-
-    return new Promise<Response<T>>((resolve, reject) => {
-      this.pendingRequests.set(this.nonce, { resolve, reject })
-      this.socket.send(JSON.stringify({ nonce: this.nonce, request }))
-    })
+  if (!this.socket.isOpened) {
+    console.warn('WARN:', `Cannot send request to unconnected peer ${this.socket.address}`)
+    return []
   }
+
+  const nonce = ++this.nonce
+
+  return new Promise<Response<T>>((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      this.pendingRequests.delete(nonce)
+      reject(new Error(`Request ${nonce} timed out after 5s`))
+    }, 15_000)
+
+    this.pendingRequests.set(nonce, {
+      resolve: (response: Response<T>) => {
+        clearTimeout(timeout)
+        resolve(response)
+      },
+      reject: (err: unknown) => {
+        clearTimeout(timeout)
+        reject(err)
+      }
+    })
+
+    this.socket.send(JSON.stringify({ nonce, request }))
+  })
+}
 
   public async announcePeer(announce: Announce) {
     if (this.socket.hostname === announce.address) return // console.log('LOG:', "Won't announce peer to itself")
